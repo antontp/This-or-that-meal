4070a8cff261d4a01f321efb07407faf1e8dbc68
Anton <anton-tin@hotmail.com>
2023-01-26 00:24:35 +0800

    fetch successfull

--- js/index.js
+++ js/index.js
@@ -1,9 +1,51 @@
 const api_url = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
-async function fetchData() {
-    var data = await (await fetch(api_url)).json();
-    console.log(data.meals[0]);
-    const pEl = document.getElementById("fetch").innerHTML = JSON.stringify(data.meals[0]);
+// Fetching DOM objects
+const x = {
+    container: document.getElementById("xDish"),
+    name: document.getElementById("xName"),
+    image: document.getElementById("xImg")
 }
 
-fetchData();
+const y = {
+    container: document.getElementById("yDish"),
+    name: document.getElementById("yName"),
+    image: document.getElementById("yImg")
+}
+
+
+async function fetchData() {
+    return await fetch(api_url);
+}
+
+async function promiseToData(promise) {
+    var data = await promise.json();
+    return data.meals[0];
+}
+
+function postMeal(dish, meal) {
+    dish.name = meal.strMeal;
+    dish.image.src = meal.strMealThumb;
+}
+// Startup function
+async function startup() {
+    fetchData()
+        .then(promiseToData)
+        .then(dish => {
+            postMeal(x, dish)
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    
+    fetchData()
+        .then(promiseToData)
+        .then(dish => {
+            postMeal(y, dish)
+        })
+        .catch(error => {
+            console.log(error)
+        });
+}
+startup();
+
